e3d591864337638367a90379c04be0bf92715d00
AlexK8900 <karpov.alexandr90@gmail.com>
2023-03-27 22:52:54 +0200

    Add some dependencies, create MainFragment

---
+++ app/src/main/java/com/example/spacexdemo/presentation/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.example.spacexdemo.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.spacexdemo.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var binding: FragmentMainBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
